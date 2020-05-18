4dc2f4d3b2d235d31eba1245b534b473d10d6912
Steve Saju <stevesajustephen@gmail.com>
2020-05-18 12:17:07 +0530

    fixed error when connecting to db.create docker-compose.yml file

--- connection.js
+++ connection.js
@@ -1,13 +1,5 @@
 const Sequelize = require("sequelize");
 const config = require("config");
 
-const database = config.get("database"); //order_app
-const username = config.get("username"); //postgres
-const password = config.get("password"); //docker
-
-const connection = new Sequelize(database, username, password, {
-  host: config.get("host"),
-  dialect: config.get("dialect"),
-});
-
+const connection = new Sequelize(process.env.DATABASE_URL);
 module.exports = connection;
